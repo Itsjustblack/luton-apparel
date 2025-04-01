4fe51be69702d37409d584d89405bc1635c52828
jason aghedo <jason.aghedo@gmail.com>
2025-04-01 18:03:21 +0100

    fix: Removed view on mobile

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,15 @@
 import { ReactLenis } from "@studio-freight/react-lenis";
 import { ReactNode } from "react";
+import ViewOnlyOnDesktop from "../ViewOnlyOnDesktop";
 
 interface Props {
 	children: ReactNode | ReactNode[];
 }
 
 export function Layout({ children }: Props) {
-	return <ReactLenis root>{children}</ReactLenis>;
+	return (
+		<ReactLenis root>
+			<ViewOnlyOnDesktop>{children}</ViewOnlyOnDesktop>
+		</ReactLenis>
+	);
 }
